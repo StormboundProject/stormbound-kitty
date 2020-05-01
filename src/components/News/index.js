c515399679de9f06fc823b6dafc5a2cedc60c5a1
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-01 10:03:05 +0200

    Dynamically fetch news

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,24 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import news from '../../data/news'
 import Column from '../Column'
 import CTA from '../CTA'
+import Error from '../Error'
+import Loader from '../Loader'
 import Row from '../Row'
 import chunk from '../../helpers/chunk'
+import useFetch from '../../hooks/useFetch'
 import './index.css'
 
 const MAX_NEWS = 7
 
 export default React.memo(function News(props) {
+  const { data: news = [], error, loading } = useFetch('data/news.json')
   const pages = chunk(news, MAX_NEWS)
   const [activePage, setActivePage] = React.useState(0)
   const loadPrev = () => setActivePage(page => page + 1)
   const loadNext = () => setActivePage(page => page - 1)
 
+  if (error) {
+    return <Error noImage noTitle error={`Error fetching the news.`} />
+  }
+
+  if (loading) {
+    return <Loader />
+  }
+
   return (
     <>
       <ul className='News'>
-        {pages[activePage].map((news, index) => (
+        {(pages[activePage] || []).map((news, index) => (
           <li className='News__item' key={index}>
             {news.link ? (
               <Link to={news.link}>
